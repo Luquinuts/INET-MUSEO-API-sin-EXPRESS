0259bffb6dcb594e38a4052f07bbb3ec206210dd
Lucas Palavecino <ljgaming15.games@gmail.com>
2022-11-02 11:18:53 -0300

    dia 3

---
+++ RouterSet.js
@@ -0,0 +1,5 @@
+export function SetRoute(){
+    typeof router.searchRoute(path, req.method) !== "undefined" 
+    ? router.searchRoute(path, req.method) 
+    : (req, res) => { res.writeHead(404); res.end('NOT FOUND') };
+}
